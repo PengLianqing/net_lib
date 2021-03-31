0990eb61db8fdb481f41bdd3bd8813484879a6cc
PengLianqing <157345800@qq.com>
2021-03-31 01:16:42 -0700

    1.多线程开始执行receive cpu占用过高，需要优化逻辑 2.单独使用协程无法回收 3.netco::co_sleep(50);需要优化，考虑通过引用计数等实现

--- client/main.cc
+++ client/main.cc
@@ -10,172 +10,45 @@
 using namespace netco;
 
 // export LD_LIBRARY_PATH=/home/peng/Share/myprj/openSourcePrj/netco/src/
+// export LD_LIBRARY_PATH=/home/peng/code/net_lib/src/
 // ./netco_test -lnetco
-
-// netco http response with one acceptor test 
-// 只有一个acceptor的服务
-void single_acceptor_server_test()
-{
-	netco::co_go(
-		[]{
-			netco::Socket listener;
-			if (listener.isUseful()) // 判断socket是否有效
-			{
-				listener.setTcpNoDelay(true); // 设置socket的tcp协议不使用Nagle算法
-				listener.setReuseAddr(true); // 地址、端口复用
-				listener.setReusePort(true);
-				if (listener.bind(8099) < 0) // 绑定地址结构
-				{
-					return;
-				}
-				listener.listen(); // 监听
-			}
-			while (1){
-				netco::Socket* conn = new netco::Socket(listener.accept()); // 接收连接
-				conn->setTcpNoDelay(true); // 设置新的socket的tcp协议不使用Nagle算法
-				netco::co_go( // 协程 执行通信
-					[conn]
-					{
-						std::vector<char> buf;
-						buf.resize(2048);
-						while (1)
-						{
-							auto readNum = conn->read((void*)&(buf[0]), buf.size());
-							std::string ok = "HTTP/1.0 200 OK\r\nServer: netco/0.1.0\r\nContent-Type: text/html\r\n\r\n";
-							if(readNum < 0){
-								break;
-							}
-							conn->send(ok.c_str(), ok.size());
-							conn->send((void*)&(buf[0]), readNum);
-							if(readNum < (int)buf.size()){
-								break;
-							}
-						}
-						netco::co_sleep(100);//需要等一下，否则还没发送完毕就关闭了
-						delete conn;
-					}
-					);
-			}
-		}
-	);
-}
-
-//netco http response with multi acceptor test 
-//每条线程一个acceptor的服务
-void multi_acceptor_server_test()
-{
-	auto tCnt = ::get_nprocs_conf();
-	for (int i = 0; i < tCnt; ++i)
-	{
-		netco::co_go(
-			[]
-			{
-				netco::Socket listener;
-				if (listener.isUseful())
-				{
-					listener.setTcpNoDelay(true);
-					listener.setReuseAddr(true);
-					listener.setReusePort(true);
-					if (listener.bind(8099) < 0)
-					{
-						return;
-					}
-					listener.listen();
-				}
-				while (1)
-				{
-					netco::Socket* conn = new netco::Socket(listener.accept());
-					conn->setTcpNoDelay(true);
-					netco::co_go(
-						[conn]
-						{
-							std::string hello("HTTP/1.0 200 OK\r\nServer: netco/0.1.0\r\nContent-Length: 72\r\nContent-Type: text/html\r\n\r\n<HTML><TITLE>hello</TITLE>\r\n<BODY><P>hello word!\r\n</BODY></HTML>\r\n");
-							//std::string hello("<HTML><TITLE>hello</TITLE>\r\n<BODY><P>hello word!\r\n</BODY></HTML>\r\n");
-							char buf[1024];
-							if (conn->read((void*)buf, 1024) > 0)
-							{
-								conn->send(hello.c_str(), hello.size());
-								netco::co_sleep(50);//需要等一下，否则还没发送完毕就关闭了
-							}
-							delete conn;
-						}
-						);
-				}
-			}
-			,parameter::coroutineStackSize, i);
-	}
-	
-}
 
 //作为客户端的测试，可配合上述server测试
 void client_test(){
-	for (int i = 0; i < 50; ++i){
-		netco::co_go(
-				[]
-				{
-					char buf[1024];
-					int j = 2000;
-					while(--j>0){
-						netco::co_sleep(1);
-						netco::Socket s;
-						s.connect("127.0.0.1", 8099);
-						s.send("ping", 4);
-						s.read(buf, 1024);
-						// std::cout << std::string(buf) << std::endl;
-					}
-					std::cout << "all done." << std::endl;
-				}
-				);
-	}
-	std::cout<<"client done."<<std::endl;
-}
-
-//读写锁测试
-void mutex_test(netco::RWMutex& mu){
-	for(int i = 0; i < 10; ++i)
-	if(i < 5){
-		netco::co_go(
-		[&mu, i]{
-			mu.rlock();
-			std::cout << i << " : start reading" << std::endl;
-			netco::co_sleep(100);
-			std::cout << i << " : finish reading" << std::endl;
-			mu.runlock();
-			mu.wlock();
-			std::cout << i << " : start writing" << std::endl;
-			netco::co_sleep(100);
-			std::cout << i << " : finish writing" << std::endl;
-			mu.wunlock();
+	netco::co_go(
+		[]{
+			for (int i = 0; i < 1; ++i){
+				netco::co_go(
+						[ i ]
+						{
+							char buf[1024];
+							int j = 10;
+							while(j-->0){
+								std::cout<<j<<std::endl; // 协程的第j个连接
+								netco::co_sleep(1); // 协程切换
+								netco::Socket s;
+								s.connect("127.0.0.1", 7103);
+								s.send("ping", 4);
+								s.read(buf, 1024);
+								// std::cout << std::string(buf) << std::endl;
+							}
+						} 
+				// );
+				,parameter::coroutineStackSize, i);
+			}
 		}
 		);
-	}else{
-		netco::co_go(
-		[&mu, i]{
-			mu.wlock();
-			std::cout << i << " : start writing" << std::endl;
-			netco::co_sleep(100);
-			std::cout << i << " : finish writing" << std::endl;
-			mu.wunlock();
-			mu.rlock();
-			std::cout << i << " : start reading" << std::endl;
-			netco::co_sleep(100);
-			std::cout << i << " : finish reading" << std::endl;
-			mu.runlock();
-		}
-	);
-	}
-	
 }
 
+/*
+问题出在不能回收线程
+不存在协程前部分执行出错的情况
+*/
 int main()
 {
-	//netco::RWMutex mu;
-	//mutex_test(mu);
-	//single_acceptor_server_test();
-	//multi_acceptor_server_test();
 	client_test();
-	std::this_thread::sleep_for( std::chrono::milliseconds(10000) );
-	//netco::sche_join();
+	// std::this_thread::sleep_for( std::chrono::milliseconds(10000) );
+	netco::sche_join();
 	std::cout << "end" << std::endl;
 	return 0;
 }
